568ae72434be7d96be78ed2d361566c4f56174f5
Annisa Mahfuzhah <101617147+AnnisaMahfuzhah@users.noreply.github.com>
2022-04-06 00:02:46 +0800

    Delete UTS/AnnisaMyTechnology directory

--- UTS/AnnisaMyTechnology/app/src/main/java/com/example/annisamytechnology/model/AnnisaMyTechnology.kt
+++
@@ -1,9 +0,0 @@
-package com.example.annisamytechnology.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class AnnisaMyTechnology (
-    @StringRes val stringResourceId : Int,
-    @DrawableRes val imageResourceId : Int,
-)
